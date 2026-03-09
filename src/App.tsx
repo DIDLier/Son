81da2c69cc5ef89426691e39c49964a48f9740e1
nile <20234448@s.ubaguio.edu>
2026-03-10 06:49:09 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { HashRouter, Routes, Route, Link } from "react-router-dom";
 import Home from './Pages/Home'
 import About from './Pages/About'
 import Service from './Pages/Service'
@@ -6,29 +6,18 @@ import Contact from './Pages/Contact'
 import Navbar from "./Components/NavBarTemplate";
 import NotFound from "./Pages/NotFound";
 
-
 function App() {
-
   return (
-    
-    <BrowserRouter>
-
-    <Navbar />
-  
+    <HashRouter>
+      <Navbar />
       <Routes>
-
-        <Route path="/" element={<Home/>} />
-
-        <Route path="/about" element={<About/>} />
-
-        <Route path="/contact" element={<Contact/>} />
-
-        <Route path="/service" element={<Service/>} />
-
-        <Route path="*" element={<NotFound/>} />
-
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
